872a8546842c24c17470042fdf85d43e2012f266
Brenda Long <brenda@studio6thave.com>
2021-03-11 20:02:27 -0600

    intial files

---
+++ scripts/legos/LegoList.js
@@ -0,0 +1,16 @@
+import { LegoDetail } from './LegoDetail.js';
+
+export const makeLegoList = (legosArray) => {
+  render(legosArray)
+};
+
+const render = (legoData) => {
+  	const legoDisplay = document.querySelector("#all-legos");
+	//What is map?
+	//what does map return? An Array.
+  	let HTMLArray = legoData.map(oneLego => {
+    	return LegoDetail(oneLego);
+  	})
+  	//Since HTMLArray is an array, we can use join to make it a string
+  	legoDisplay.innerHTML = HTMLArray.join("");
+}
